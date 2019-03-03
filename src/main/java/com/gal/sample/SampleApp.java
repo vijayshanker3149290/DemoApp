c8d10c1122e202ed7b0f5ed54947c6b2ee76cdee
vijay shanker <vijayshanker1786@gmail.com>
2019-03-03 19:22:16 +0530

    third Commit

--- src/main/java/com/gal/sample/SampleApp.java
+++ src/main/java/com/gal/sample/SampleApp.java
@@ -4,7 +4,10 @@ public class SampleApp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-	System.out.println("hi");
+		System.out.println("hi");
+		int a=10;
+		int b=15;
+		System.out.println("The sume of two numbers are:"+(a+b));
 	}
 
 }
