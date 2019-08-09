865296c937f09114a829e49b94a1db7cab31dee1
Vijay Shanker <vijay.shanker@nagarro.com>
2019-08-09 16:26:50 +0530

    Created a new branch

---
+++ src/main/java/com/gal/sample/SampleApp3.java
@@ -0,0 +1,10 @@
+package com.gal.sample;
+
+public class SampleApp3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub		
+		System.out.println("Trying to create a new branch.");
+	}
+
+}
